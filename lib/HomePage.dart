f2fbdfaaa69266f91ecfe581c8b4129a10084890
José <perezg.josesal@gmail.com>
2021-06-24 02:26:44 -0500

    DropdownMenu added and shared_preferences removed

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,7 +2,7 @@ import 'package:coffe_counter_app/CoffeesProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +13,16 @@ class HomePage extends StatelessWidget{
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             SizedBox(width: double.infinity),
-            Icon(Icons.coffee,size: 80,color: Colors.white,),
-            Text(8.toString(),
-            style: TextStyle(color: Colors.white),),
+            Icon(
+              Icons.coffee,
+              size: 80,
+              color: Colors.white,
+            ),
+            Text(
+              8.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            _CoffeeSizeButtonsRow()
           ],
         ),
       ),
@@ -30,12 +37,33 @@ class _CoffeeSizeButtonsRow extends StatefulWidget {
 
 class __CoffeeSizeButtonsRowState extends State<_CoffeeSizeButtonsRow> {
   bool isChecked = false;
+  dynamic valueItem = 'Small';
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Checkbox(value: true, onChanged: (value){},)
-      ],
+    return DropdownButton<String>(
+      items: _getDropItems(),
+      dropdownColor: Colors.brown,
+      value: valueItem,
+      style: TextStyle(color: Colors.white, fontSize: 25),
+      onChanged: (e) {
+        setState(() {
+          valueItem = e;
+        });
+      },
     );
+  }
+
+  List<DropdownMenuItem<String>> _getDropItems() {
+    List<String> sizes = ['Small', 'Medium', 'Large'];
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (int i = 0; i < sizes.length; i++) {
+      String size = sizes[i];
+      DropdownMenuItem<String> dropDownItem = DropdownMenuItem<String>(
+        child: Text(size),
+        value: size,
+      );
+      dropDownItems.add(dropDownItem);
+    }
+    return dropDownItems;
   }
 }
